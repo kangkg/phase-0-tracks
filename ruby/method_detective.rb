33d5e9d8b9c8d04f97f14e50c542133fc55e082b
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2016-09-19 18:11:56 -0500

    First commit- finished 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-### "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts "zom".replace "zoom" #not sure if this is what they want
 # => “zoom”
 
-# "enhance".<???>
+puts "enhance".center("enhance".length + 8, " ")
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop #could use byteslice ..
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".byteslice(1..-1) #could also use an array..? or slice!(0)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".ord # I believe this is the character encoding ASCII decimal-code-number. opposite is chr
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "This is the opposite of \"z\".ord: #{122.chr}" 
+
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
