527a09b0a37c908d336eb1b5da9c73481edcf8fc
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2016-09-28 21:01:59 -0500

    Working through setter-getter.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,21 +1,53 @@
-def class Santa
+class Santa 
+
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
 
-  def eat_milk_and_cookies (cookie)
+  def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}!" 
   end
 
-  def initialize
-    puts "Initializing Santa instance ..."
-    
+  # Setter Methods: 
+  def celebrate_birthday #should age Santa by one year.
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name) # take a reindeer's name as an argument, and move that reindeer in last place in the reindeer rankings.
+    @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name))) # NOT CERTAIN IF THIS WORKS
+    @reindeer_ranking.push(reindeer_name)
+  end
+
+  def gender=(new_gender) # The @gender attribute should have a setter method that allows @gender to be reassigned from outside the class definition.
+    @gender = new_gender  
   end
 
 end #end class
 
+########################## Driver Code #########################
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
 
-# gender, which will be a string passed in on initialization
-# ethnicity, which will be a string passed in on initialization
-# reindeer_ranking, an array of reindeer from most preferred to least preferred. This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-# age, which is not passed in on initialization and defaults to 0
+santas2 = Santa.new("tester", "oranger")
+
+p santas2
+
+
+
+# Add two "getter" methods as well:
+
+# The method age should simply return @age.
+# The method ethnicity should return @ethnicity.
+# Update your driver code to test your work.
