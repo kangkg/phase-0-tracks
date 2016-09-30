652f22ee766997b22615d06202747ef1fb053233
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2016-09-30 10:33:51 -0500

    Made some more fixes.

--- ruby/myWordGuessingGame/game.rb
+++ ruby/myWordGuessingGame/game.rb
@@ -3,62 +3,41 @@ class WordGuessingGame
   attr_reader :correct_answer, :done, :updated_guess, :number_of_guesses, :guess_limit ########## probably remove correct answer later
 
   def initialize (correct_answer) #add class-arguments here
-    @correct_answer = correct_answer 
-    @updated_guess = ""; correct_answer.length.times { @updated_guess << "_ " }; @updated_guess.strip!
-    @guess_limit = @correct_answer.length
-    @number_of_guesses = 0
-    @done = nil
-    @past_guesses = []
+    @correct_answer = correct_answer
+    @updated_guess = ""; correct_answer.length.times { @updated_guess << "_ " }
+    @guess_limit = @correct_answer.length # set the total-amount of guesses possible
+    @number_of_guesses = 0                # counter for user's guess count
+    @done = nil                           # simply to say this class is done.
+    @past_guesses = []                    # hold past guesses
   end
 
   def get_user_guess(user_answer)
     if @past_guesses.include? user_answer # => to check past guesses
-      puts "You have already entered that word before."
+      puts "You have already entered that word/letter before."
       return nil # to skip to next iteration
     end
-
     @number_of_guesses += 1
-    if user_answer == @correct_answer || @correct_answer == @updated_guess.gsub(" ", "") #check if correct answer, or correct guesses in the end
-      successMessage 
-      return @done = 'done'
-    elsif @number_of_guesses == @guess_limit
-      p @updated_guess.gsub(" ", "").strip ############# DEBUGGING
-      defeatMessage
-      return @done = 'done'
-    elsif
-      ## code that updates info of updated_guess. need to be careful because .index only gets the first instance of a letter. Doesn't work if lengths mismatch. doesn't work if there's more than one letter instance in the correct word.
-      # user_answer.each_char do |user_letter| 
-      #   correct_answer.each_char do |correct_letter|
-      #     if user_letter == correct_letter
-      #       @updated_guess[correct_answer.index(correct_letter) * 2] = correct_letter
-      #     end
-      #   end
-      # end
-
-      ###### broken code
-      # if correct_answer.include? user_answer
-      #   loop_index = 0
-      #   until loop_index == correct_answer.length 
-      #     #match letter groupings
-      #     if correct_answer[loop_index] == user_answer
-      #       loop_index += 1
-      #     end
-      #   end
-      # end
-
-      # only give hint if a letter
-
-       puts "Your current guesses include these letters: #{@updated_guess}"
-       @past_guesses << user_answer
-    end # => end if-statement 
+    if user_answer == @correct_answer #check if correct answer
+      return successMessage
+    elsif (user_answer.length == 1) && (correct_answer.include? user_answer) # only give hint if a letter
+      array_of_same_letter = (0 ... @correct_answer.length).find_all {|i| @correct_answer[i,1] == user_answer } #=> gives array of all indexes of user_input's letter-instances
+      array_of_same_letter.each { |i| @updated_guess[i * 2] =  user_answer } # use the array above to fix every instance.
+      return successMessage if @correct_answer == @updated_guess.gsub(" ", "") # if new word-updated equals our answer!
+    elsif @number_of_guesses == @guess_limit # USER DEFEATED. out of guesses!
+      return defeatMessage
+    end  # => end if-statement
+    puts "Your current guesses include these letters: #{@updated_guess}"
+    @past_guesses << user_answer # store guesses
   end
 
   def successMessage
-    puts "----------------","You won in #{@number_of_guesses} guesses! ^-^"
+    puts "","----------------","You won in #{@number_of_guesses} guesses! The word was: #{@correct_answer}. ^-^"
+    @done = 'done'
   end
 
   def defeatMessage
-    puts "----------------","You lost! The word was: #{@correct_answer}."
+    puts "","----------------","You lost! The word was: #{@correct_answer}."
+    @done = 'done'
   end
 
 end #=> end class
@@ -66,16 +45,17 @@ end #=> end class
 ########################## Driver Code #########################
 puts "First user, please enter in the word to guess:"
 print "> "
-new_game = WordGuessingGame.new('un'.downcase.strip) # will use gets.chomp or "STDIN.noecho(&:gets).chomp" later instead of unicorn
+new_game = WordGuessingGame.new(gets.chomp.downcase.strip) #('unicorn'.downcase.strip) # will use gets.chomp or "STDIN.noecho(&:gets).chomp" later instead of unicorn
 
-until new_game.done == "done"
-  puts "Second user, please guess what single letters are within the answer (for a hint), or guess the entire answer:", "(#{ new_game.guess_limit - new_game.number_of_guesses } guesses left.)"
+until new_game.done == "done" # not sure if i should make a method instead of this driver-code
+  puts "","Second user, guess what single letters are within the answer (for a hint), or guess the entire answer:", "(#{ new_game.guess_limit - new_game.number_of_guesses } guesses left.)"
   print "> "
-  new_game.get_user_guess(gets.chomp) # this doesn't work with RSPEC.....
+  new_game.get_user_guess(gets.chomp) # gets.chomp doesn't work with RSPEC.....
 end
 
 
 
+# => i can tell invalid length
 
 # One user can enter a word (or phrase, if you would like your game to support that), and another user attempts to guess the word.
 # Guesses are limited, and the number of guesses available is related to the length of the word.
--- ruby/myWordGuessingGame/game_spec.rb
+++ ruby/myWordGuessingGame/game_spec.rb
@@ -8,7 +8,12 @@ describe WordGuessingGame do
   end
 
   it "stores the correct answer in underscores, given on initialization." do
-    expect(guess_my_answer.updated_guess).to eq "_ _ _ _ _ _ _ " #unicorn is 7.
+    expect(guess_my_answer.updated_guess).to eq "_ _ _ _ _ _ _ " #unicorn is 7 length.
   end
 
+  it "takes a user's letter and adds a hint if applicable." do
+    expect(get_user_guess('u')).to eq "u _ _ _ _ _ _ " 
+  end
 end
+
+# Surely I could fix this rspec to take gets.chomp. In addition, I could make the rspec test random alphabetical letters.
