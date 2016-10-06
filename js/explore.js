fed14c03a01bf8650625a8987374234f6502c3ef
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2016-10-06 18:46:43 -0500

    Finished 7.1

---
+++ js/explore.js
@@ -0,0 +1,22 @@
+// was i supposed to make my own..?
+function reverseMe(str) {
+  return str.split("").reverse().join("");
+}
+
+// alternative way:
+// for (var i = str.length - 1; i >= 0; i--) { 
+//     reversedWord = reversedWord + str[i]; 
+// } return reversedWord
+
+
+
+
+// Driver; run with node filename.js
+var answer = reverseMe("Hello!!!")
+if (1 == 1) {
+  console.log(answer);
+}
+
+
+
+// take string. go from -1 and delete at 1.. for length of loop
