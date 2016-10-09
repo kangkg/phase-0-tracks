a65b9a411f400bfa771817d24b1053dc19201760
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2016-10-08 19:26:37 -0500

    Finished release-0

--- js/algos.js
+++ js/algos.js
@@ -1,8 +1,9 @@
-// In algos.js, write a function that takes an array of words or phrases and returns the longest word or phrase in the array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase". This is a logical reasoning challenge, so try to reason through this based on what you already know, rather than Googling the fanciest solution or looking up built-in sorting functions. Note that "some string".length will do what you expect in JS.
-// Add driver code that tests your function on a few arrays.
-
-
-
+// In algos.js, write a function that takes two objects and checks to see if the objects share at least one key-value pair. (You'll keep adding to this file, so we suggest grouping functions at the top and testing at the bottom of the file.) If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, the function would return true, because at least one key-value pair matches between the two objects. If no pairs match (and keep in mind that the two objects may not even have any of the same keys), the function should return false. To make your life easier, don't worry about whether a property is a string ('age') or an identifier name (age). Those can be considered equivalent. Again, try to reason through the problem using the basics you've already learned, rather than looking up slick search functions that will do the job for you. We'd rather see you write code that you actually understand!
+// Add some driver code that tests both outcomes of your function.
+function shares_pair?(first_obj, second_obj) {
+  
+  return false // if no matching pairs
+}
 
 
 function find_longest_phrase(array_of_strings) {
@@ -18,5 +19,8 @@ function find_longest_phrase(array_of_strings) {
 
 // Driver code:
 
+// Release-0
 console.log("The Longest Phrase was: " + find_longest_phrase(["long phrase","longest phrase","longer phrase"]))
 console.log("The Longest Phrase was: " + find_longest_phrase(["WOWWWWWWW","NOO OOO OOO OO","WHYYYY MEEEEEEEEE","WUTTTT","OMG"]))
+
+// release-1
---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+color_array = ["blue", "red", "orange", "yellow"];
+horse_array = ["Johnny", "Matey", "Freddy", "Baby"];
+
+color_array.push("radiant purple");
+horse_array.push("Joey");
+
+//Write additional code that adds another color to the colors array, and another horse name to the horses array.
